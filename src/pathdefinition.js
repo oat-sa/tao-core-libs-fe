e19a6128c6b0f8fd8f38f4ec858d1c87c99b16f4
Tamas Besenyei <tamas@besenyei.net>
2019-07-30 15:44:28 +0200

    add requirejs path definition

---
+++ src/pathdefinition.js
@@ -0,0 +1,14 @@
+requirejs.config({
+    paths: {
+        jquery: '/node_modules/jquery/jquery',
+        lodash: '/node_modules/lodash/lodash',
+        moment: '/node_modules/moment/min/moment-with-locales',
+        handlebars: '/node_modules/handlebars/dist/handlebars.amd',
+        tpl: '/node_modules/@oat-sa/tao-core-libs/dist/tpl',
+        'lib/dompurify/purify': '/node_modules/dompurify/dist/purify',
+        'lib/moment-timezone.min': '/node_modules/moment-timezone/builds/moment-timezone-with-data.min',
+        async: '/node_modules/async/lib/async',
+
+        lib: '/node_modules/@oat-sa/tao-core-libs/dist'
+    }
+});
