029265c4a7df74eb2c00cb7be27e9205491bd01a
Tamas Besenyei <tamas@besenyei.net>
2019-07-30 16:19:26 +0200

    update dep list and paths

--- src/pathdefinition.js
+++ src/pathdefinition.js
@@ -7,8 +7,13 @@ requirejs.config({
         tpl: '/node_modules/@oat-sa/tao-core-libs/dist/tpl',
         'lib/dompurify/purify': '/node_modules/dompurify/dist/purify',
         'lib/moment-timezone.min': '/node_modules/moment-timezone/builds/moment-timezone-with-data.min',
+        'lib/expr-eval/expr-eval': '/node_modules/@oat-sa/expr-eval/dist/bundle',
         async: '/node_modules/async/lib/async',
+        'lib/decimal/decimal': '/node_modules/decimal.js/decimal',
 
+        // locals
+        'lib/unmatrix': '/node_modules/@oat-sa/tao-core-libs/dist/unmatrix/unmatrix',
+        class: '/node_modules/@oat-sa/tao-core-libs/dist',
         lib: '/node_modules/@oat-sa/tao-core-libs/dist'
     }
 });
