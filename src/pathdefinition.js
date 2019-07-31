f4a5d537db675014638a0a3187d583b516b00ea2
Tamas Besenyei <tamas@besenyei.net>
2019-07-31 08:17:03 +0200

    path definitions to object

--- src/pathdefinition.js
+++ src/pathdefinition.js
@@ -1,24 +1,22 @@
-requirejs.config({
-    paths: {
-        jquery: '/node_modules/jquery/jquery',
-        lodash: '/node_modules/lodash/lodash',
-        moment: '/node_modules/moment/min/moment-with-locales',
-        handlebars: '/node_modules/handlebars/dist/handlebars.amd',
-        tpl: '/node_modules/@oat-sa/tao-core-libs/dist/tpl',
-        'lib/dompurify/purify': '/node_modules/dompurify/dist/purify',
-        'lib/moment-timezone.min': '/node_modules/moment-timezone/builds/moment-timezone-with-data.min',
-        'lib/expr-eval/expr-eval': '/node_modules/@oat-sa/expr-eval/dist/bundle',
-        async: '/node_modules/async/lib/async',
-        'lib/decimal/decimal': '/node_modules/decimal.js/decimal',
-        'lib/gamp/gamp': '/node_modules/gamp/src/gamp',
-        select2: '/node_modules/select2/select2',
-        interact: '/node_modules/interactjs/dist/interact',
-        'lib/popper/tooltip': '/node_modules/tooltip.js/dist/umd/tooltip',
-        popper: '/node_modules/popper.js/dist/umd/popper',
+export default {
+    jquery: '/node_modules/jquery/jquery',
+    lodash: '/node_modules/lodash/lodash',
+    moment: '/node_modules/moment/min/moment-with-locales',
+    handlebars: '/node_modules/handlebars/dist/handlebars.amd',
+    tpl: '/node_modules/@oat-sa/tao-core-libs/dist/tpl',
+    'lib/dompurify/purify': '/node_modules/dompurify/dist/purify',
+    'lib/moment-timezone.min': '/node_modules/moment-timezone/builds/moment-timezone-with-data.min',
+    'lib/expr-eval/expr-eval': '/node_modules/@oat-sa/expr-eval/dist/bundle',
+    async: '/node_modules/async/lib/async',
+    'lib/decimal/decimal': '/node_modules/decimal.js/decimal',
+    'lib/gamp/gamp': '/node_modules/gamp/src/gamp',
+    select2: '/node_modules/select2/select2',
+    interact: '/node_modules/interactjs/dist/interact',
+    'lib/popper/tooltip': '/node_modules/tooltip.js/dist/umd/tooltip',
+    popper: '/node_modules/popper.js/dist/umd/popper',
 
-        // locals
-        'lib/unmatrix': '/node_modules/@oat-sa/tao-core-libs/dist/unmatrix',
-        class: '/node_modules/@oat-sa/tao-core-libs/dist/class',
-        lib: '/node_modules/@oat-sa/tao-core-libs/dist'
-    }
-});
+    // locals
+    'lib/unmatrix': '/node_modules/@oat-sa/tao-core-libs/dist/unmatrix',
+    class: '/node_modules/@oat-sa/tao-core-libs/dist/class',
+    lib: '/node_modules/@oat-sa/tao-core-libs/dist'
+};
