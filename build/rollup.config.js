6257c3844a18f2d2d9bc0a5eb40648995b578699
andrii.ponamarov <ponamarevandrej@gmail.com>
2020-03-19 14:51:17 +0100

    Add wilcard external plugin

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -20,6 +20,7 @@ import path from 'path';
 import glob from 'glob-promise';
 import alias from 'rollup-plugin-alias';
 import handlebarsPlugin from 'rollup-plugin-handlebars-plus';
+import wildcardExternalPlugin from '@oat-sa/rollup-plugin-wildcard-external';
 import copy from 'rollup-plugin-copy';
 import { copyFile, mkdirp } from 'fs-extra';
 const Handlebars = require('handlebars');
@@ -58,56 +59,9 @@ export default inputs.map(input => {
             dir: path.join(outputDir, dir),
             format: 'amd'
         },
-        external: [
-            'jquery',
-            'lodash',
-            'i18n',
-            'lib/gamp/gamp',
-            'handlebars',
-            'lib/dompurify/purify',
-            'raphael',
-            'select2-origin/select2',
-            'select2-origin/select2_locale_ar',
-            'select2-origin/select2_locale_bg',
-            'select2-origin/select2_locale_ca',
-            'select2-origin/select2_locale_cs',
-            'select2-origin/select2_locale_da',
-            'select2-origin/select2_locale_de',
-            'select2-origin/select2_locale_el',
-            'select2-origin/select2_locale_es',
-            'select2-origin/select2_locale_et',
-            'select2-origin/select2_locale_eu',
-            'select2-origin/select2_locale_fa',
-            'select2-origin/select2_locale_fi',
-            'select2-origin/select2_locale_fr',
-            'select2-origin/select2_locale_gl',
-            'select2-origin/select2_locale_he',
-            'select2-origin/select2_locale_hr',
-            'select2-origin/select2_locale_hu',
-            'select2-origin/select2_locale_id',
-            'select2-origin/select2_locale_is',
-            'select2-origin/select2_locale_it',
-            'select2-origin/select2_locale_ja',
-            'select2-origin/select2_locale_ko',
-            'select2-origin/select2_locale_lt',
-            'select2-origin/select2_locale_lv',
-            'select2-origin/select2_locale_mk',
-            'select2-origin/select2_locale_ms',
-            'select2-origin/select2_locale_nl',
-            'select2-origin/select2_locale_no',
-            'select2-origin/select2_locale_pl',
-            'select2-origin/select2_locale_pt-BR',
-            'select2-origin/select2_locale_pt-PT',
-            'select2-origin/select2_locale_ro',
-            'select2-origin/select2_locale_ru',
-            'select2-origin/select2_locale_sk',
-            'select2-origin/select2_locale_sv',
-            'select2-origin/select2_locale_th',
-            'select2-origin/select2_locale_tr',
-            'select2-origin/select2_locale_vi',
-            'select2-origin/select2_locale_zh-CN'
-        ],
+        external: ['jquery', 'lodash', 'i18n', 'lib/gamp/gamp', 'handlebars', 'lib/dompurify/purify', 'raphael'],
         plugins: [
+            wildcardExternalPlugin(['select2-origin/**']),
             alias({
                 resolve: ['.js', '.json', '.tpl'],
                 ...aliases
