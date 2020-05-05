64b93b5a418b2a645b6e26ec585befbe9097f5e8
Hanna Dzmitryieva <bziondik@gmail.com>
2020-05-05 13:42:34 +0300

    return select2-origin

--- src/pathdefinition.js
+++ src/pathdefinition.js
@@ -15,13 +15,14 @@ export default {
     popper: '/node_modules/popper.js/dist/umd/popper',
     raphael: '/node_modules/raphael/raphael',
     select2: '/node_modules/select2/select2',
-    ckeditor: '/node_modules/@oat-sa/tao-core-libs/lib/ckeditor/ckeditor',
-    'lib/simulator': '/node_modules/@oat-sa/tao-core-libs/lib/simulator',
+    'select2-origin': '/node_modules/select2',
 
     // locals
     class: '/node_modules/@oat-sa/tao-core-libs/dist/class',
     'scale.raphael': '/node_modules/@oat-sa/tao-core-libs/dist/raphael/scale.raphael',
     nouislider: '/node_modules/@oat-sa/tao-core-libs/dist/sliders/jquery.nouislider',
     iframeNotifier: '/node_modules/@oat-sa/tao-core-libs/dist/iframeNotifier',
+    ckeditor: '/node_modules/@oat-sa/tao-core-libs/lib/ckeditor/ckeditor',
+    'lib/simulator': '/node_modules/@oat-sa/tao-core-libs/lib/simulator',
     lib: '/node_modules/@oat-sa/tao-core-libs/dist'
 };
