ee97226964a758639abfb33ce8361e02bed38f8c
Bertrand Chevrier <chevrier.bertrand@gmail.com>
2020-06-23 09:50:10 +0200

    add select2 to the path definitions

--- src/pathdefinition.js
+++ src/pathdefinition.js
@@ -15,6 +15,7 @@ export default {
     popper: '/node_modules/popper.js/dist/umd/popper',
     raphael: '/node_modules/raphael/raphael',
     'select2-origin': '/node_modules/select2',
+    select2: '/node_modules/select2',
 
     // locals
     class: '/node_modules/@oat-sa/tao-core-libs/dist/class',
