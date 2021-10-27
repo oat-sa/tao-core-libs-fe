aeb98850158e61c89bbdef498103ec5930cf7f2a
Yuriy Synyaiev <yuriy.synyaiev@taotesting.com>
2021-10-27 15:16:32 +0200

    feat!: major version update for select 2

--- src/select2.js
+++ src/select2.js
@@ -11,120 +11,120 @@ var lang = getUserLanguage();
 
 switch (lang) {
     case 'ar':
-        import('select2-origin/select2_locale_ar');
+        import('select2-origin/select2-origin/i18n/ar');
         break;
     case 'bg':
-        import('select2-origin/select2_locale_bg');
+        import('select2-origin/select2-origin/i18n/bg');
         break;
     case 'ca':
-        import('select2-origin/select2_locale_ca');
+        import('select2-origin/select2-origin/i18n/ca');
         break;
     case 'cs':
-        import('select2-origin/select2_locale_cs');
+        import('select2-origin/select2-origin/i18n/cs');
         break;
     case 'da':
-        import('select2-origin/select2_locale_da');
+        import('select2-origin/select2-origin/i18n/da');
         break;
     case 'de':
-        import('select2-origin/select2_locale_de');
+        import('select2-origin/select2-origin/i18n/de');
         break;
     case 'el':
-        import('select2-origin/select2_locale_el');
+        import('select2-origin/select2-origin/i18n/el');
         break;
     case 'es':
-        import('select2-origin/select2_locale_es');
+        import('select2-origin/select2-origin/i18n/es');
         break;
     case 'et':
-        import('select2-origin/select2_locale_et');
+        import('select2-origin/select2-origin/i18n/et');
         break;
     case 'eu':
-        import('select2-origin/select2_locale_eu');
+        import('select2-origin/select2-origin/i18n/eu');
         break;
     case 'fa':
-        import('select2-origin/select2_locale_fa');
+        import('select2-origin/select2-origin/i18n/fa');
         break;
     case 'fi':
-        import('select2-origin/select2_locale_fi');
+        import('select2-origin/select2-origin/i18n/fi');
         break;
     case 'fr':
-        import('select2-origin/select2_locale_fr');
+        import('select2-origin/select2-origin/i18n/fr');
         break;
     case 'gl':
-        import('select2-origin/select2_locale_gl');
+        import('select2-origin/select2-origin/i18n/gl');
         break;
     case 'he':
-        import('select2-origin/select2_locale_he');
+        import('select2-origin/select2-origin/i18n/he');
         break;
     case 'hr':
-        import('select2-origin/select2_locale_hr');
+        import('select2-origin/select2-origin/i18n/hr');
         break;
     case 'hu':
-        import('select2-origin/select2_locale_hu');
+        import('select2-origin/select2-origin/i18n/hu');
         break;
     case 'id':
-        import('select2-origin/select2_locale_id');
+        import('select2-origin/select2-origin/i18n/id');
         break;
     case 'is':
-        import('select2-origin/select2_locale_is');
+        import('select2-origin/select2-origin/i18n/is');
         break;
     case 'it':
-        import('select2-origin/select2_locale_it');
+        import('select2-origin/select2-origin/i18n/it');
         break;
     case 'ja':
-        import('select2-origin/select2_locale_ja');
+        import('select2-origin/select2-origin/i18n/ja');
         break;
     case 'ko':
-        import('select2-origin/select2_locale_ko');
+        import('select2-origin/select2-origin/i18n/ko');
         break;
     case 'lt':
-        import('select2-origin/select2_locale_lt');
+        import('select2-origin/select2-origin/i18n/lt');
         break;
     case 'lv':
-        import('select2-origin/select2_locale_lv');
+        import('select2-origin/select2-origin/i18n/lv');
         break;
     case 'mk':
-        import('select2-origin/select2_locale_mk');
+        import('select2-origin/select2-origin/i18n/mk');
         break;
     case 'ms':
-        import('select2-origin/select2_locale_ms');
+        import('select2-origin/select2-origin/i18n/ms');
         break;
     case 'nl':
-        import('select2-origin/select2_locale_nl');
+        import('select2-origin/select2-origin/i18n/nl');
         break;
     case 'no':
-        import('select2-origin/select2_locale_no');
+        import('select2-origin/select2-origin/i18n/nb');
         break;
     case 'pl':
-        import('select2-origin/select2_locale_pl');
+        import('select2-origin/select2-origin/i18n/pl');
         break;
     case 'br':
-        import('select2-origin/select2_locale_pt-BR');
+        import('select2-origin/select2-origin/i18n/pt-BR');
         break;
     case 'pt':
-        import('select2-origin/select2_locale_pt-PT');
+        import('select2-origin/select2-origin/i18n/pt');
         break;
     case 'ro':
-        import('select2-origin/select2_locale_ro');
+        import('select2-origin/select2-origin/i18n/ro');
         break;
     case 'ru':
-        import('select2-origin/select2_locale_ru');
+        import('select2-origin/select2-origin/i18n/ru');
         break;
     case 'sk':
-        import('select2-origin/select2_locale_sk');
+        import('select2-origin/select2-origin/i18n/sk');
         break;
     case 'sv':
-        import('select2-origin/select2_locale_sv');
+        import('select2-origin/select2-origin/i18n/sv');
         break;
     case 'th':
-        import('select2-origin/select2_locale_th');
+        import('select2-origin/select2-origin/i18n/th');
         break;
     case 'tr':
-        import('select2-origin/select2_locale_tr');
+        import('select2-origin/select2-origin/i18n/tr');
         break;
     case 'vi':
-        import('select2-origin/select2_locale_vi');
+        import('select2-origin/select2-origin/i18n/vi');
         break;
     case 'zh':
-        import('select2-origin/select2_locale_zh-CN');
+        import('select2-origin/select2-origin/i18n/zh-CN');
         break;
 }
