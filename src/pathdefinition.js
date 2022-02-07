fccc4bf404509b394e460ad8e464aee589332808
Martin Nicholson <martin@taotesting.com>
2022-02-07 16:56:41 +0100

    fix: point to non-AMD Handlebars

--- src/pathdefinition.js
+++ src/pathdefinition.js
@@ -2,7 +2,7 @@ export default {
     jquery: '/node_modules/jquery/jquery',
     lodash: '/node_modules/lodash/lodash',
     moment: '/node_modules/moment/min/moment-with-locales',
-    handlebars: '/node_modules/handlebars/dist/handlebars.amd',
+    handlebars: '/node_modules/handlebars/dist/handlebars',
     tpl: '/node_modules/@oat-sa/tao-core-libs/dist/tpl',
     'lib/dompurify/purify': '/node_modules/dompurify/dist/purify',
     'lib/moment-timezone.min': '/node_modules/moment-timezone/builds/moment-timezone-with-data.min',
