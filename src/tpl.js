aac377386e4c7bc53f82f684f6fc9436ce831b15
jsconan <jsconan@gmail.com>
2022-11-21 17:34:07 +0100

    feat: externalise the module writer

--- src/tpl.js
+++ src/tpl.js
@@ -13,7 +13,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  *
- * Copyright (c) 2013-2019 (original work) Open Assessment Technologies SA (under the project TAO-PRODUCT);
+ * Copyright (c) 2013-2022 (original work) Open Assessment Technologies SA (under the project TAO-PRODUCT);
  */
 
 /**
@@ -29,6 +29,7 @@
  */
 import hb from 'handlebars';
 import handlebarsHelpers from 'lib/handlebars/helpers';
+import moduleWriter from 'lib/handlebars/moduleWriter';
 
 handlebarsHelpers(hb);
 
@@ -61,11 +62,7 @@ export default {
         if (moduleName in buildMap) {
             const compiled = hb.precompile(buildMap[moduleName]);
             // Write out precompiled version of the template function as AMD definition.
-            write(
-                `define('tpl!${moduleName}', ['handlebars'], function(hb){ \n` +
-                    `return hb.template(${compiled.toString()});\n` +
-                    `});\n`
-            );
+            write(moduleWriter(moduleName, compiled));
         }
     }
 };
